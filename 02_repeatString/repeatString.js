a2b08a2da228f96f93672c076c8b888c2b299abe
Enaya Jawed <jawedenaya@gmail.com>
2025-04-17 13:44:44 -0500

    completed exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeats) {
+  let returnString = "";
+  if(numRepeats<0){
+    return 'ERROR';
+  } else {
+    for(let i=0; i<numRepeats; i++){
+      returnString += string;
+    }
+  }
+  return returnString;
 };
 
 // Do not edit below this line
